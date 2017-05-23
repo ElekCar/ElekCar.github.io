af5462b4a1476d1c2dd2260b56b9148f05418c5c
ElekCar <tuur.lobel@sgsintpaulus.eu>
2017-05-23 15:28:23 +0200

    Add files via upload

---
+++ hallo.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.applet.*;
+import java.awt.*;
+
+public class hallo extends Applet {
+   public void paint(Graphics g) {
+      g.drawString("Welcome in Java Applet.",40,20);
+   }
+}
